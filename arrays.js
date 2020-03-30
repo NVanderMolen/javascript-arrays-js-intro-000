f1f61bcc56e6baa7ed07a61f41764a3c506a904c
Nick VanderMolen <tree.vandermolen@gmail.com>
2020-03-30 22:03:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,16 @@ function addElementToBeginningOfArray(arr, element){
 var newArray = [element,...arr]
 return newArray
 }
+
 function destructivelyAddElementToBeginningOfArray(arr, element){
   arr.unshift(element);
+  return arr
+}
+function addElementToEndOfArray(arr, element){
+  var newArray = [...arr, element]
+  return newArray;
+}
+function destructivelyAddElementToEndOfArray (arr, element){
+  arr.push(element);
   return arr
 }
