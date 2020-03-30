e4c4b1a955f73863f0ca5f72268b76ce14c54fac
Nick VanderMolen <tree.vandermolen@gmail.com>
2020-03-30 21:10:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(arr, element){
-  var arr = []; 
+  var arr = [];
   addElementToBeginningOfArray.unshift(element);
 }
